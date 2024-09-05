c427f759727296447a4cf19c9865422ca042cc37
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2024-09-05 23:16:24 +0530

    comments in javascript

---
+++ week 01 - basics/01_comments.js
@@ -0,0 +1,21 @@
+// Comments
+/*
+1. Comments are nothing but used to read the code.
+2. Comments are ignored while the execution of program.
+3. There are 2 types of comments :- (single, multiline).
+*/
+
+// single line comments
+// let x = 2;
+// let y = 3;
+// let z = 4;
+
+// multi line comments
+
+const value = 2;
+if (value == 0) {
+    console.log(`${value} is zero.`);
+} else {
+    console.log(`${value} is greater than zero.`);
+    
+}
