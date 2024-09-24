89ab01b5287064fb1959244859d37c67877be34f
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2024-09-24 22:55:11 +0530

    conversion of datatypes

---
+++ week 01 - basics/04_conversion.js
@@ -0,0 +1,38 @@
+// Conversion of datatypes
+/*
+1. We can convert from one datatype to another using "typeof" - inbuilt function.
+*/
+
+
+let score = "sourabh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let userName = "sourabh"
+
+let booleanUserName = Boolean(userName)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
