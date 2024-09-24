d88e4d77cf4985f89aa05bdd0432a584396e28a7
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2024-09-24 23:18:11 +0530

    worked on strings

---
+++ week 01 - basics/07_strings.js
@@ -0,0 +1,38 @@
+// Strings in JS
+
+const userName = "Sourabh"
+const repoCount = 30
+
+// console.log(userName + repoCount + " Value");
+
+
+// use backticks
+// console.log(`Hello my name is ${userName} and my repo count is ${repoCount}`);
+
+const gameName = new String('googleRunner.com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   sourabh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
