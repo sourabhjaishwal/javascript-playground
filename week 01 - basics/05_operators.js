cb45c3bed5ff2e43a007e5b8cf0e459f21a44fd5
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2024-09-24 22:55:44 +0530

    operators in js

---
+++ week 01 - basics/05_operators.js
@@ -0,0 +1,76 @@
+// Operators
+/*
+Javascript operators are used to perform different types of mathematical and logical computations.
+*/
+
+
+// negative value
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// arithmetic operators
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// concatination of two strings
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+// comparison opeartors
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// double & triple equality
+// (loose & strict checking)
+
+console.log("2" == 2);
+
+console.log("2" === 2);
